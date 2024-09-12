0e218a78261775652969f339e27cd4ebf3f1aaca
Chamara-Wijepala <mr.worf777@gmail.com>
2024-09-12 18:23:30 +0530

    Fix issues with using hosted DB

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,13 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-	host: process.env.HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DATABASE,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	// host: process.env.HOST,
+	// port: process.env.DB_PORT,
+	// database: process.env.DATABASE,
+	// user: process.env.USER,
+	// password: process.env.PASSWORD,
+	// ssl: {
+	// 	require: true,
+	// },
+	connectionString: process.env.PGURI,
 });
--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const getAllMessages = `SELECT * FROM messages`;
 
 const insertMessage = `
-  INSERT INTO messages (username, text, date) VALUES ($1, $2, $3)
+  INSERT INTO messages (username, message_body, date_added) VALUES ($1, $2, $3)
 `;
 
 module.exports = {
